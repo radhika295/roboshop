3abdca2a1b3af955d36acca4d49692aaf15446c8
Radhika <sairadhika.iiitn@gmail.com>
2025-09-23 21:50:15 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR :: Please run this scirpt with root privilage $N"
+    exit 1
+fi
+LOGS_FOLDER="/var/log/shell-roboshop"
+
+mkdir -p $LOGS_FOLDER
+
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+#mkdir -p $LOGS_FOLDER
+
+echo "Script satrted executed at: $(date)" | tee -a $LOG_FILE
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+         echo -e "$2 is installed successfully: $G Success $N" | tee -a $LOG_FILE
+         
+    else    
+         echo -e "$2 is not installed it is failed: $R Failed $N" | tee -a $LOG_FILE
+         exit 1
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "Adding Repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+
+VALIDATE $? "Mongodb Installed"
+
+systemctl enable mongod &>>$LOG_FILE
+
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod &>>$LOG_FILE
+
+VALIDATE $? "start MongoDB"
