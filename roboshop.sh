a20e8ec0cd124042c543783295aee1e99f8404d1
Radhika <sairadhika.iiitn@gmail.com>
2025-09-23 16:51:34 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ SG_ID="sg-045897fa6d8ffd258"
 for instance in $@
 do
     Instance_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-    if [ $instance != "frontend" ]; the
+    if [ $instance != "frontend" ]; then
         Private_Ip=$(aws ec2 describe-instances --instance-ids $Instance_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
         Private_Ip=$(aws ec2 describe-instances --instance-ids $Instance_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
