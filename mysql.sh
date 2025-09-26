05003cc94dbd4ab359d9a740f686c1811247b03e
Radhika <sairadhika.iiitn@gmail.com>
2025-09-26 23:41:55 +0530

    roboshop

---
+++ mysql.sh
@@ -0,0 +1,52 @@
+!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR :: Please run this scirpt with root privilage $N"
+    exit 1
+fi
+
+END_TIME=$(date +%s)
+
+SCRIPT_DIR=$PWD
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+
+mkdir -p $LOGS_FOLDER
+
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+#mkdir -p $LOGS_FOLDER
+
+echo "Script satrted executed at: $(date)" | tee -a $LOG_FILE
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+         echo -e "$2  : $G Success $N" | tee -a $LOG_FILE
+         
+    else    
+         echo -e "$2  : $R Failed $N" | tee -a $LOG_FILE
+         exit 1
+    fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MYSQL"
+
+systemctl enable mysqld
+VALIDATE $? "Enabled MYSQL"
+
+systemctl start mysqld  
+VALIDATE $? "Started MYSQL"
+
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
